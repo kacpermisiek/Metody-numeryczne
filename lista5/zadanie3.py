761194f541ce806480ab2e2b2f7d4b76b6352369
kacpermisiek <315560@uwr.edu.pl>
2021-01-19 10:25:35 +0100

    zadanie3 i zadanie4 poprawa

--- lista5/zadanie3.py
+++ lista5/zadanie3.py
@@ -1,23 +1,28 @@
-from scipy.interpolate import PchipInterpolator
+from scipy.interpolate import CubicSpline
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-xs = np.array([0.2, 2.0, 20.0, 200.0, 2000.0, 20000.0])
-ys = np.array([103.0, 13.9, 2.72, 0.8, 0.401, 0.433])
+R_e = np.array([0.2, 2.0, 20.0, 200.0, 2000.0, 20000.0])
+C_d = np.array([103.0, 13.9, 2.72, 0.8, 0.401, 0.433])
 
 
-pch = PchipInterpolator(xs, ys)
+cs = CubicSpline(np.log(R_e), np.log(C_d))
 
-results = [pch(5.5), pch(5000)]
-print(f"f(5.5) = {results[0]}\nf(5000) = {results[1]}")
+R_e_list = [5, 50, 5000]
+C_d_list = np.exp(cs(np.log(R_e_list)))
 
-plt.scatter(xs, ys)
-x_range = np.arange(0.2, 20000, 0.001)
+x_range = np.arange(0.1, 21000, 1)
 
-plt.scatter([5.5, 5000], results)
-plt.plot(x_range, pch(x_range))
-plt.xscale('log', base=2)
-plt.yscale('log', base=2)
+plt.scatter(R_e, C_d, label='Punkty z danych')
+plt.scatter(R_e_list, C_d_list, label='Obliczone punkty')
+plt.plot(x_range, np.exp(cs(np.log(x_range))))
+
+for i in range(len(R_e_list)):
+    print(f"Wartosc Cd dla Re = {R_e_list[i]}  :  {C_d_list[i]}")
+
+plt.legend()
+plt.xscale('log')
+plt.yscale('log')
 plt.grid()
 plt.show()
--- lista5/zadanie4.py
+++ lista5/zadanie4.py
@@ -1,27 +1,26 @@
+from scipy.interpolate import lagrange
 import matplotlib.pyplot as plt
 import numpy as np
 
-xs = np.array([0.2, 2.0, 20.0, 200.0, 2000.0, 20000.0])
-ys = np.array([103.0, 13.9, 2.72, 0.8, 0.401, 0.433])
+R_e = np.array([0.2, 2.0, 20.0, 200.0, 2000.0, 20000.0])
+C_d = np.array([103.0, 13.9, 2.72, 0.8, 0.401, 0.433])
 
-plt.scatter(xs, ys)
+lag = lagrange(np.log(R_e), np.log(C_d))
 
-f = np.polyfit(xs, ys, 1)
-f1 = np.poly1d(f)
+x_range = np.arange(0.1, 25000, 0.1)
 
-print(f1)
+R_e_list = [5, 50, 5000]
+C_d_list = np.exp(lag(np.log(R_e_list)))
 
-Re_x = [5.5, 5000]
-Re_y = [f1(Re_x[0]), f1(Re_x[1])]
+plt.scatter(R_e, C_d, label='Punkty z danych')
+plt.scatter(R_e_list, C_d_list, label='Obliczone punkty')
+plt.plot(x_range, np.exp(lag(np.log(x_range))))
 
+for i in range(len(R_e_list)):
+    print(f"Wartosc Cd dla Re = {R_e_list[i]}  :  {C_d_list[i]}")
 
-x_range = np.arange(0.2, 20000, 0.001)
-
-print(f"f(5.5) = {Re_y[0]}\nf(5000) = {Re_y[1]}")
-
-plt.scatter(Re_x, Re_y)
-plt.plot(x_range, f1(x_range))
-plt.xscale('log', base=2)
-plt.yscale('log', base=2)
+plt.legend()
+plt.xscale('log')
+plt.yscale('log')
 plt.grid()
 plt.show()
