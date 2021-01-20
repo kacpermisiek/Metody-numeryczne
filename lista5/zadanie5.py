33a3b09258ddd1ddb52ba848126d218d20862356
kacpermisiek <315560@uwr.edu.pl>
2021-01-20 13:43:33 +0100

    update z3 z4 z5 z6

--- lista5/zadanie5.py
+++ lista5/zadanie5.py
@@ -6,14 +6,18 @@ y = np.array([6.008, 15.772, 27.13, 33.772, 5.257, 9.549, 11.098, 28.828])
 
 x_range = np.arange(1, 4, 0.001)
 
-poly1 = np.polyfit(x, y, 1)
+poly1, tres1, trank1, tsv1, trcond1 = np.polyfit(x, y, 1, full=True)
 fun1 = np.poly1d(poly1)
 y1 = fun1(x_range)
 
-poly2 = np.polyfit(x, y, 2)
+poly2, tres2, trank2, tsv2, trcond2 = np.polyfit(x, y, 2, full=True)
 fun2 = np.poly1d(poly2)
 y2 = fun2(x_range)
 
+print(f"suma kwadratow odchylenia od funkcji liniowa: {tres1}")
+print(f"suma kwadratow odchylenia od funkcji kwadratowa: {tres2}")
+
+print("\nODP. Lepiej dopasowana jest funkcja kwadratowa ")
 
 plt.scatter(x, y)
 plt.plot(x_range, y1, label='linia prosta')
--- lista5/zadanie6.py
+++ lista5/zadanie6.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 h = np.array([0, 1.525, 3.05, 4.575, 6.1, 7.625, 9.150])
 ro = np.array([1.0, 0.8617, 0.7385, 0.6292, 0.5328, 0.4481, 0.3741])
 
 z = np.polyfit(h, ro, 2)
 p = np.poly1d(z)
+x_range = np.linspace(0, 10, 1000)
+
+plt.plot(x_range, p(x_range), label='wykres funkcji')
 
 print(f'p(10.5km) = {p(10.5)}')
 
