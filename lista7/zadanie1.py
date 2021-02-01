3b2861a1b82b948e27c4c9e5443f78f09a8d3a64
kacpermisiek <315560@uwr.edu.pl>
2021-02-01 16:10:28 +0100

    dodano zadanie 1 z 7 listy

---
+++ lista7/zadanie1.py
@@ -0,0 +1,20 @@
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def f1(t, y):
+    return np.sin(t*y)
+
+
+y0=[2, 2.5, 3, 3.5]
+
+for el in y0:
+    wynik = solve_ivp(f1, [0,6], [el])
+    print('\nWyniki dla y0 równego',el)
+    print('t=', wynik.t)
+    print('y=', wynik.y[0])
+    plt.plot(wynik.t, wynik.y[0],'o-')
+
+plt.legend(['y0=2', 'y0=2.5', 'y0=3', 'y0=3.5'])
+plt.show()
