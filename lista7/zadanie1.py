31ce741188d11e777eff2ca580c17c64c2a3fbd7
kacpermisiek <315560@uwr.edu.pl>
2021-02-03 13:27:18 +0100

    l7z1 update
    zamiast punktow wykreslana jest funkcja

--- lista7/zadanie1.py
+++ lista7/zadanie1.py
@@ -7,14 +7,15 @@ def f1(t, y):
     return np.sin(t*y)
 
 
-y0=[2, 2.5, 3, 3.5]
+y0 = [2, 2.5, 3, 3.5]
 
 for el in y0:
-    wynik = solve_ivp(f1, [0,6], [el])
-    print('\nWyniki dla y0 równego',el)
-    print('t=', wynik.t)
-    print('y=', wynik.y[0])
-    plt.plot(wynik.t, wynik.y[0],'o-')
+    wynik = solve_ivp(f1, [0, 6], [el], atol=1e-12, rtol=1e-9)
+    print('\nWyniki dla y0 równego', el)
+    # print('t=', wynik.t)
+    # print('y=', wynik.y[0])
+    plt.plot(wynik.t, wynik.y[0])
 
 plt.legend(['y0=2', 'y0=2.5', 'y0=3', 'y0=3.5'])
+plt.grid()
 plt.show()
