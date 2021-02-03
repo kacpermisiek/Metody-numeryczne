1326df61ddbd931e53ce9521f5a3a8b6755d510e
kacpermisiek <315560@uwr.edu.pl>
2021-02-03 13:27:44 +0100

    l8 z3 update
    poprawione zle wypisywanie wartosci i wektorow wlasnych

--- lista8/zadanie3.py
+++ lista8/zadanie3.py
@@ -4,10 +4,17 @@ import numpy as np
 n_array = [10, 100]
 
 for n in n_array:
+    print(f"\n n = {n}\n\n")
     d = 2 * np.ones(n)
     nd = -1 * np.ones(n-1)
     w3, v3 = eigh_tridiagonal(d, nd)
 
-    print("Wartości własne: ", w3[:3], sep="\n")
-    print("Wektory własne: ", v3[:, :3], sep="\n")
+    print("Wartosci wlasne: ")
+    for i in range(3):
+        print(w3[i])
+
+    print("Wektory wlasne: ")
+    for i in range(3):
+        print(v3[i])
+
 
